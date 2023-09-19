cfe3d0b4af35b87d00f2d8001bcba2b7d216a84e
zatzby <128881293+zatzby@users.noreply.github.com>
2023-09-18 23:16:19 -0700

    adjusted landing page and info to guides page
    enabled typewriter script, and added links/adjusted layout to guides +page.svelte

--- src/routes/guides/+page.svelte
+++ src/routes/guides/+page.svelte
@@ -7,30 +7,35 @@
       <span slot="header">Edgebander</span>
       <p class="mb-2 text-gray-500 dark:text-gray-400">The Jade 240 edgebander is key to our operation. Responsible for the edges of our cabinet boxes, fronts, shelves and more.</p>
       <p class="text-gray-500 dark:text-gray-400">
-        Check out this guide to learn how to <a href="/" target="_blank" rel="noreferrer" class="text-blue-600 dark:text-blue-500 hover:underline"> start up the machine</a>.
+        View the <a href="/" target="_blank" rel="noferrer" class="text-blue-600 dark:text-blue-500 hover:underline">manual.</a>
       </p>
-      <p class="text-gray-500 dark:text-gray-400">
-        View this guide to learn how to <a href="/" target="_blank"
-        rel="noreferrer" class="text-blue-600 dark:text-blue-500
-        hover:underline"> adjust and calibrate</a> the stations for production.
-      </p>
-      <p class="text-gray-500 dark:text-gray-400">
-        Watch this video to see how to <a href="/" target="_blank"
-        rel="noreferrer" class="text-blue-600 dark:text-blue-500
-        hover:underline"> clean the glue pot</a> 
-        .
-    </p>
+      <ul class="list-disc pl-5 dark:text-gray-400 text-gray-500">
+        <li>
+          <a href="/" target="_blank" rel="noreferrer" class="text-blue-600 dark:text-blue-500 hover:underline"> Start up the machine.</a>
+        </li>
+        <li>
+          <a href="/" target="_blank" rel="noferrer" class="text-blue-600 dark:text-blue-500 hover:underline">Adjust and calibrate the stations.</a>
+        </li>
+        <li>
+          <a href="https://tailwindui.com/" rel="noreferrer" target="_blank" class="text-blue-600 dark:text-blue-500 hover:underline">Clean the glue pot.</a>
+        </li>
+      </ul>
     </AccordionItem>
     <AccordionItem>
       <span slot="header">Router</span>
       <p class="mb-2 text-gray-500 dark:text-gray-400">The Rover CNC Router is our tried and true workhorse. It cuts and machines panel components for each of our projects.</p>
+      <p class="text-gray-500 dark:text-gray-400">
+        View the <a href="/" target="_blank" rel="noferrer" class="text-blue-600 dark:text-blue-500 hover:underline">manual.</a>
       <p class="mb-2 text-gray-500 dark:text-gray-400">Learn more about our router:</p>
       <ul class="list-disc pl-5 dark:text-gray-400 text-gray-500">
         <li>
-          <a href="/" target="_blank" rel="noreferrer" class="text-blue-600 dark:text-blue-500 hover:underline"> Lorem ipsum </a>
+          <a href="/" target="_blank" rel="noreferrer" class="text-blue-600 dark:text-blue-500 hover:underline"> Start up the machine.</a>
         </li>
         <li>
-          <a href="https://tailwindui.com/" rel="noreferrer" target="_blank" class="text-blue-600 dark:text-blue-500 hover:underline"> Tailwind UI </a>
+          <a href="/" target="_blank" rel="noferrer" class="text-blue-600 dark:text-blue-500 hover:underline">View maintenance tasks.</a>
+        </li>
+        <li>
+          <a href="https://tailwindui.com/" rel="noreferrer" target="_blank" class="text-blue-600 dark:text-blue-500 hover:underline"> Prepare a new spoilboard.</a>
         </li>
       </ul>
     </AccordionItem>
